2793828513eebecb5a29b0b23b2428cfb6379339
wangsir0623 <1636801376@qq.com>
2017-02-23 15:55:27 +0800

    modify the StreamSelectLoop.php

--- EventLoop/EventLoopInterface.php
+++ EventLoop/EventLoopInterface.php
@@ -2,5 +2,80 @@
 namespace EventLoop;
 
 interface EventLoopInterface {
+    /**
+     * Read event flag
+     * @const int
+     */
+    const EV_READ = 1;
 
+    /**
+     * Write event flag
+     * @const int
+     */
+    const EV_WRITE = 2;
+
+    /**
+     * Exception event flag
+     * @const int
+     */
+    const EV_EXCEPTION = 4;
+
+    /**
+     * Signal event flag
+     * @const int
+     */
+    const EV_SIGNAL = 8;
+
+    /**
+     * Timer event flag
+     * @const int
+     */
+    const EV_TIMER = 16;
+
+    /**
+     * Once timer event flag
+     * @const int
+     */
+    const EV_TIMER_ONCE = 32;
+
+    /**
+     * add the event to the event loop
+     * @param $fd  the file descriptor. Notice: when the $flag parameter is EV_TIMER or EV_TIMER_ONCE, this paramemter means the timed task interval in seconds
+     * @param $flag  the monitored event flag. the available values are listed below
+     * EV_READ
+     * EV_WRITE
+     * EV_EXCEPTION
+     * EV_SIGNAL
+     * EV_TIMER
+     * EV_TIMER_ONCE
+     * @param $callback  the callback triggered when the event occurs. this callback has two parameters.
+     * $fd
+     * $args
+     * @param $mixed $args  the seconde argument of the callback
+     * @return bool
+     */
+    function add($fd, $flag, $callback, $args = null);
+
+    /**
+     * cancle monitoring the event
+     * @param $fd  the file descriptor. Notice: when the flag is EV_TIMER or EV_TIMER_ONCE, the $fd parameter means the id of the timed task to be deleted
+     * @param $flag  the event flag
+     * @return bool
+     */
+    function delete($fd, $flag);
+
+    /**
+     * clear all timed tasks
+     */
+    function clearAllTimer();
+
+    /**
+     * run the event loop
+     */
+    function run();
+
+    /**
+     * stop the event loop
+     */
+    function stop();
 }
--- EventLoop/StreamSelectLoop.php
+++ EventLoop/StreamSelectLoop.php
@@ -1,170 +1,218 @@
 <?php
 namespace EventLoop;
 
-use RuntimeException;
+use SplPriorityQueue;
 
 class StreamSelectLoop implements EventLoopInterface {
-    /*
-     * const int
-     * 每秒对应的毫秒数
-     */
-    const MILLISEC_PER_SEC = 1000;
-
     /**
-     * const int
-     * 每秒对应的微秒数
-     */
-    const MICROSEC_PER_SEC = 1000000;
-
-    /**
+     * whether the loop is running
      * @var bool
-     * Event loop是否进行对IO缓冲区的监视
      */
     private $running;
 
     /**
+     * all events
      * @var array
      */
-    private $readStreams = null;
+    private $all_events;
 
     /**
+     * read fds
      * @var array
      */
-    private $writeStreams = null;
+    private $read_fds;
 
     /**
+     * write fds
      * @var array
      */
-    private $exceptionStreams = null;
+    private $write_fds;
 
     /**
+     * exception fds
      * @var array
      */
-    private $timeout = array('sec' => null, 'micro_sec' => 0);  //单位为毫秒
+    private $exception_fds;
 
     /**
+     * the time shedule
+     * @var SplPriorityQueue
+     */
+    private $schedule;
+
+    /**
+     * the lasted timed task id
+     * @var int
+     */
+    private $time_id = 0;
+
+    /**
+     * the timed task array
      * @var array
      */
-    private $readStreamCallbacks = array();
+    private $tasks;
 
     /**
-     * @var array
+     * the select loop timeout in microseconds
+     * @var int
      */
-    private $writeStreamCallbacks = array();
+    private $select_timeout = 1000000;
 
-    /**
-     * @var array
-     */
-    private $exceptionStreamCallbacks = array();
-
-    /**
-     * @var callable
-     */
-    private $timeoutCallback;
-
-    /**
-     * 添加一个流并监视其输入缓冲区
-     * @param $stream  要监视的流
-     * @param $callback  当输入缓冲区有数据时调用的回调函数
-     * @param null $args  调用回调函数时，会传递两个参数，第一个为stream,第二个即为$args
-     */
-    public function addReadStream($stream, $callback, $args = null) {
-        $key = (int)$stream;
-
-        //添加要监视的流和回调函数
-        $this->readStreams[$key] = $stream;
-        $this->readStreamCallbacks[$key] = compact(array('callback', 'args'));
+    public function __construct() {
+        $this->schedule = new SplPriorityQueue();
+        $this->schedule->setExtractFlags(SplPriorityQueue::EXTR_BOTH);
     }
 
     /**
-     * 添加一个流并监视其输出缓冲区
-     * @param $stream  要监视的流
-     * @param $callback  当输出缓冲区有数据时调用的函数
-     * @param null $args  调用回调函数时，会传递两个参数，第一个为stream,第二个即为$args
+     * add the event to the event loop
+     * @param $fd  the file descriptor. Notice: when the $flag parameter is EV_TIMER or EV_TIMER_ONCE, this paramemter means the timed task interval in seconds
+     * @param $flag  the monitored event flag. the available values are listed below
+     * EV_READ
+     * EV_WRITE
+     * EV_EXCEPTION
+     * EV_SIGNAL
+     * EV_TIMER
+     * EV_TIMER_ONCE
+     * @param $callback  the callback triggered when the event occurs. this callback has two parameters.
+     * $fd
+     * $args
+     * @param $mixed $args  the seconde argument of the callback
+     * @return bool
      */
-    public function addWriteStream($stream, $callback, $args = null) {
-        $key = (int)$stream;
+    public function add($fd, $flag, $callback, $args = null) {
+        $key = (int)$fd;
 
-        //添加要监视的流和回调函数
-        $this->writeStreams[$key] = $stream;
-        $this->writeStreamCallbacks[$key] = compact(array('callback', 'args'));
-    }
+        switch ($flag) {
+            case self::EV_READ:
+                $this->all_events[$key][$flag] = array($callback, $fd, $args);
+                $this->read_fds[$key] = $fd;
 
-    /**
-     * 添加一个流并监视其输出缓冲区，如果缓冲区收到OOB数据，则触发回调函数
-     * @param $stream  要监视的流
-     * @param $callback  接收到OOB数据时调用的函数
-     * @param null $args  调用回调函数时，会传递两个参数，第一个为stream,第二个即为$args
-     */
-    public function addExceptionStream($stream, $callback, $args = null) {
-        $key = (int)$stream;
+                break;
+            case self::EV_WRITE:
+                $this->all_events[$key][$flag] = array($callback, $fd, $args);
+                $this->write_fds[$key] = $fd;
 
-        //添加要监视的流和回调函数
-        $this->exceptiontreams[$key] = $stream;
-        $this->exceptionStreamCallbacks[$key] = compact(array('callback', 'args'));
-    }
+                break;
+            case self::EV_EXCEPTION:
+                $this->all_events[$key][$flag] = array($callback, $fd, $args);
+                $this->exception_fds[$key] = $fd;
 
-    /**
-     * 设置stream_select最大时间，如果超过这个时间还没有事件发生，则会触发超时回调函数
-     * @param $timeout  超时时间，单位为微秒
-     * @param $callback  超时回调函数
-     * @param $args  在超时回调函数调用时，此参数将作为回调函数的第一个参数
-     */
-    public function setTimeout($timeout, $callback = null, $args = null) {
-        $this->timeout = $this->parseTimeout($timeout);
+                break;
+            case self::EV_SIGNAL:
+                break;
+            case self::EV_TIMER:
+            case self::EV_TIMER_ONCE;
+                //when the flag is EV_TIMER OR EV_TIMER_ONCE, the $fd parameter means the interval instead of file descriptor
 
-        $this->timeoutCallback = compact(array('callback', 'args'));
-    }
+                $run_time = microtime(true) + $fd;
+                $time_id = ++$this->time_id;
+                $this->schedule->insert($time_id, -$run_time);
+                $this->tasks[$time_id] = array($callback, $fd, $args, $flag);
 
-    /**
-     * 取消对流IO输入缓冲区的监视
-     * @param resourse $stream
-     */
-    public function removeReadStream($stream) {
-        $key = (int)$stream;
-
-        if(isset($this->readStreams[$key])) {
-            unset($this->readStreams[$key]);
-            unset($this->readStreamCallbacks[$key]);
+                $this->tick();
+                break;
+            default:
+                return false;
         }
+
+        return true;
     }
 
     /**
-     * 取消对流IO输出缓冲区的监视
-     * @param resource $stream
+     * examine whether there are tasks timed out
      */
-    public function removeWriteStream($stream) {
-        $key = (int)$stream;
+    public function tick() {
+        while(!$this->schedule->isEmpty()) {
+            $incoming = $this->schedule->top();
+            $time_id = $incoming['data'];
+            $running_time = -$incoming['priority'];
+            $now_time = microtime(true);
+            $this->select_timeout = ($running_time - $now_time) * 1000000;
 
-        if(isset($this->writeStreams[$key])) {
-            unset($this->writeStreams[$key]);
-            unset($this->writeStreamCallbacks[$key]);
+            if ($this->select_timeout <= 0) {
+                $this->schedule->extract();
+
+                if(!isset($this->tasks[$time_id])) {
+                    continue;
+                }
+
+                $task_data = $this->tasks[$time_id];
+                call_user_func($task_data[0], $task_data[1], $task_data[2]);
+                if($task_data[3] == self::EV_TIMER) {
+                    $running_time = $now_time + $task_data[1];
+                    $this->schedule->insert($time_id, -$running_time);
+                } else if($task_data[3] == self::EV_TIMER_ONCE) {
+                    $this->delete($time_id, self::EV_TIMER_ONCE);
+                }
+
+                continue;
+            }
+
+            return;
         }
+
+        //if there is no timed tasks, wu could set the stream_select() timeout longer
+        $this->select_timeout = 1000000000;
     }
 
     /**
-     * 取消对流接收OOB数据事件的监视
-     * @param resource $stream
+     * cancle monitoring the event
+     * @param $fd  the file descriptor. Notice: when the flag is EV_TIMER or EV_TIMER_ONCE, the $fd parameter means the id of the timed task to be deleted
+     * @param $flag  the event flag
+     * @return bool
      */
-    public function removeExceptionStream($stream) {
-        $key = (int)$stream;
+    public function delete($fd, $flag) {
+        $key = (int)$fd;
 
-        if(isset($this->exceptionStreams[$key])) {
-            unset($this->exceptionStreams[$key]);
-            unset($this->exceptionStreamCallbacks[$key]);
+        switch ($flag) {
+            case self::EV_READ:
+                unset($this->read_fds[$key], $this->all_events[$key][$flag]);
+                if(empty($this->all_events[$key])) {
+                    unset($this->all_events[$key]);
+                }
+
+                break;
+            case self::EV_WRITE:
+                unset($this->write_fds[$key], $this->all_events[$key][$flag]);
+                if(empty($this->all_events[$key])) {
+                    unset($this->all_events[$key]);
+                }
+
+                break;
+            case self::EV_EXCEPTION:
+                unset($this->exception_fds[$key], $this->all_events[$key][$flag]);
+                if(empty($this->all_events[$key])) {
+                    unset($this->all_events[$key]);
+                }
+
+                break;
+            case self::EV_SIGNAL:
+                break;
+            case self::EV_TIMER:
+            case self::EV_TIMER_ONCE:
+                unset($this->tasks[$key]);
+
+                break;
+            default:
+                return false;
         }
+
+        return true;
     }
 
     /**
-     * 清除event loop的超时设置，event loop在没有接收到事件时，将会一直阻塞
+     * clear all timed tasks
      */
-    public function clearTimeout() {
-        $timeout = array('sec' => null, 'micro_sec' => 0);
-        $this->timeoutCallback = null;
+    public function clearAllTimer() {
+        $this->schedule = new SplPriorityQueue();
+        $this->schedule->setExtractFlags(SplPriorityQueue::EXTR_BOTH);
+        $this->time_id = 0;
+        $this->tasks = array();
+        $this->select_timeout = 1000000000;
     }
 
     /**
-     * 开始监视
+     * run the event loop
      */
     public function run() {
         $this->running = true;
@@ -174,54 +222,46 @@ class StreamSelectLoop implements EventLoopInterface {
                 break;
             }
 
-            $readStreams = $this->readStreams;
-            $writeStreams = $this->writeStreams;
-            $exceptionStreams = $this->exceptionStreams;
+            $read_fds = $this->read_fds;
+            $write_fds = $this->write_fds;
+            $exception_fds = $this->exception_fds;
 
-            if(($num_stream_changed = stream_select($readStreams, $writeStreams, $exceptionStreams, $this->timeout['sec'], $this->timeout['micro_sec'])) === false) {
-                throw new RuntimeException('stream_select() failed.');
-            } else if($num_stream_changed > 0) {
-                foreach((array)$readStreams as $readStream) {
-                    $key = (int)$readStream;
+            stream_select($read_fds, $write_fds, $exception_fds, 0, $this->select_timeout);
 
-                    call_user_func($this->readStreamCallbacks[$key]['callback'], $readStream, $this->readStreamCallbacks[$key]['args']);
+            if(!$this->schedule->isEmpty()) {
+                $this->tick();
+            }
+
+            if(!empty($read_fds)) {
+                foreach($read_fds as $fd) {
+                    $event = $this->all_events[(int)$fd][self::EV_READ];
+
+                    call_user_func($event[0], $event[1], $event[2]);
                 }
+            }
 
-                foreach((array)$writeStreams as $writeStream) {
-                    $key = (int)$writeStream;
+            if(!empty($write_fds)) {
+                foreach($write_fds as $fd) {
+                    $event = $this->all_events[(int)$fd][self::EV_WRITE];
 
-                    call_user_func($this->writeStreamCallbacks[$key]['callback'], $writeStream, $this->writeStreamCallbacks[$key]['args']);
+                    call_user_func($event[0], $event[1], $event[2]);
                 }
+            }
 
-                foreach((array)$exceptionStreams as $exceptionStream) {
-                    $key = (int)$exceptionStream;
+            if(!empty($exception_fds)) {
+                foreach($exception_fds as $fd) {
+                    $event = $this->all_events[(int)$fd][self::EV_EXCEPTION];
 
-                    call_user_func($this->exceptionStreamCallbacks[$key]['callback'], $exceptionStream, $this->exceptionStreamCallbacks[$key]['args']);
-                }
-            } else {
-                //超时
-                if(is_callable($this->timeoutCallback['callback'])) {
-                    call_user_func($this->timeoutCallback['callback'], $this->timeoutCallback['args']);
+                    call_user_func($event[0], $event[1], $event[2]);
                 }
             }
         }
     }
 
     /**
-     * 停止监视
+     * stop the event loop
      */
     public function stop() {
         $this->running = false;
-    }
-
-    /**
-     * 将毫秒单位的时间转换成需要的格式
-     * @param int $timeout
-     */
-    private function parseTimeout($timeout) {
-        $sec = ceil($timeout / self::MICROSEC_PER_SEC);
-        $micro_sec = $timeout - $sec*self::MICROSEC_PER_SEC;
-
-        return compact(array('sec', 'micro_sec'));
     }
 }
