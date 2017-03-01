b26dea8a087355016ffac4bfb3603ba24bcb8a3d
wangsir0623 <1636801376@qq.com>
2017-03-01 16:32:58 +0800

    LibEventLoop.php

--- EventLoop/LibEventLoop.php
+++ EventLoop/LibEventLoop.php
@@ -2,72 +2,192 @@
 namespace EventLoop;
 
 class LibEventLoop implements EventLoopInterface {
-    private $base;
-
+    /**
+     * whether the loop is running
+     * @var bool
+     */
     private $running;
 
-    private $events = array();
+    /**
+     * the event base
+     * @var resource
+     */
+    private $base;
+
+    /**
+     * read/write events
+     * @var array
+     */
+    private $events;
+
+    /**
+     * signal events
+     * @var array
+     */
+    private $signals;
+
+    /**
+     * timer events
+     * @var array
+     */
+    private $timers;
 
     public function __construct() {
         $this->base = event_base_new();
     }
 
-    private function addStream($stream, $events, $callback, $args = null) {
-        $key = (int)$stream;
+    /**
+     * add the event to the event loop
+     * @param $fd  the file descriptor. Notice: when the $flag parameter is EV_TIMER or EV_TIMER_ONCE, this paramemter means the timed task interval in seconds
+     * @param $flag  the monitored event flag. the available values are listed below
+     * EV_READ
+     * EV_WRITE
+     * EV_SIGNAL
+     * EV_TIMER
+     * EV_TIMER_ONCE
+     * @param $callback  the callback triggered when the event occurs. this callback has three parameters.
+     * $fd
+     * $events
+     * $args
+     * @param $mixed $args  the third argument of the callback
+     * @return bool
+     */
+    public function add($fd, $flag, $callback, $args = null) {
+        switch($flag) {
+            case self::EV_SIGNAL:
+                $key = (int)$fd;
 
-        $this->removeStream($stream, $events);
+                $event = event_new();
+                if(!event_set($event, $fd, EV_SIGNAL | EV_PERSIST, $callback, $args)) {
+                    return false;
+                }
+                if(!event_base_set($event, $this->base)) {
+                    return false;
+                }
+                if(!event_add($event)) {
+                    return false;
+                }
 
-        $event = event_new();
-        event_set($event, $stream, $events, $callback, $args);
-        event_base_set($event, $this->base);
-        event_add($event);
+                $this->signals[$key] = $event;
 
-        $this->events[$key][$events] = $event;
+                break;
+            case self::EV_TIMER:
+            case self::EV_TIMER_ONCE:
+                $event = event_new();
+                $key = (int)$event;
+
+                if(!event_timer_set($event, array($this, 'timerCallback'), $key)) {
+                    return false;
+                }
+                if(!event_base_set($event, $this->base)) {
+                    return false;
+                }
+                if(!event_add($event, $fd * 1000000)) {
+                    return false;
+                }
+
+                $this->timers[$key] = array($event, $fd, $flag, $callback, $args);
+                break;
+            case self::EV_READ:
+            case self::EV_WRITE:
+                $real_flag = ($flag === self::EV_READ ? EV_READ : EV_WRITE) | EV_PERSIST;
+
+                $key = (int)$fd;
+
+                $event = event_new();
+                if(!event_set($event, $fd, $real_flag, $callback, $args)) {
+                    return false;
+                }
+                if(!event_base_set($event, $this->base)) {
+                    return false;
+                }
+                if(!event_add($event)) {
+                    return false;
+                }
+
+                $this->events[$key][$flag] = $event;
+                break;
+            default:
+                return false;
+        }
+
+        return true;
     }
 
-    private function removeStream($stream, $events) {
-        $key = (int)$stream;
+    /**
+     * cancle monitoring the event
+     * @param $fd  the file descriptor. Notice: when the flag is EV_TIMER or EV_TIMER_ONCE, the $fd parameter means the event id of the timed task to be deleted
+     * @param $flag  the event flag
+     * @return bool
+     */
+    public function delete($fd, $flag) {
+        switch($flag) {
+            case self::EV_SIGNAL:
+                $key = (int)$fd;
+                if(!empty($this->signals[$key])) {
+                    event_del($this->signals[$key]);
+                    unset($this->signals[$key]);
+                }
 
-        $events |= EV_PERSIST;
+                break;
+            case self::EV_TIMER:
+            case self::EV_TIMER_ONCE:
+                $key = (int)$fd;
+                if(!empty($this->timers[$key])) {
+                    event_del($this->timers[$key][0]);
+                    unset($this->timers[$key]);
+                }
 
-        if(isset($this->events[$key][$events])) {
-            event_del($this->events[$key][$events]);
+                break;
+            case self::EV_READ:
+            case self::EV_WRITE:
+                $key = (int)$fd;
+                if(!empty($this->events[$key][$flag])) {
+                    event_del($this->events[$key][$flag]);
+                    unset($this->events[$key][$flag]);
+                }
+
+                if(empty($this->events[$key])) {
+                    unset($this->events[$key]);
+                }
+                break;
+            default:
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * clear all timed tasks
+     */
+    public function clearAllTimer() {
+        foreach($this->timers as $timer) {
+            event_del($timer[0]);
+        }
+
+        $this->timers = array();
+    }
+
+    /**
+     * run the timer callback. when the event flag is TIMER, re-add the timer event to the event loop
+     * @param $fd
+     * @param $events
+     * @param $key
+     */
+    private function timerCallback($fd, $events, $key) {
+        call_user_func($this->timers[$key][3], null, EV_TIMEOUT, $this->timers[$key][4]);
+
+        if($this->timers[$key][2] == self::EV_TIMER) {
+            event_add($this->timers[$key][0], $this->timers[$key][1] * 1000000);
+        } else {
+            $this->delete($key, self::EV_TIMER_ONCE);
         }
     }
 
-    public function addReadStream($stream, $callback, $args = null, $once = false) {
-        $key = (int)$stream;
-
-        $events = EV_READ;
-        if(!$once) {
-            $events |= EV_PERSIST;
-        }
-
-        $this->addStream($stream, $events, $callback, $args);
-    }
-
-    public function addWriteStream($stream, $callback, $args = null, $once = false) {
-        $key = (int)$stream;
-
-        $events = EV_WRITE;
-        if(!$once) {
-            $events |= EV_PERSIST;
-        }
-
-        $this->addStream($stream, $events, $callback, $args);
-    }
-
-    public function addTimer($timeout, $callback, $args = null) {
-    }
-
-    public function removeReadStream($stream) {
-        $this->removeStream($stream, EV_READ);
-    }
-
-    public function removeWriteStream($stream) {
-        $this->removeStream($stream, EV_WRITE);
-    }
-
+    /**
+     * run the event loop
+     */
     public function run() {
         $this->running = true;
 
@@ -80,6 +200,9 @@ class LibEventLoop implements EventLoopInterface {
         }
     }
 
+    /**
+     * stop the event loop
+     */
     public function stop() {
         $this->running = false;
     }
